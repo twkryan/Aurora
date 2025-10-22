aef0a9d8c6e048aaa1d44eb0f7614a99249c41e9
Lucas Souza <lucasxx.brito@gmail.com>
2025-10-21 23:11:43 -0300

    correção landing page, tres paginas de temas adicionados e header

--- js/preloader.js
+++ js/preloader.js
@@ -3,5 +3,5 @@
     // delay opcional para deixar a animação completa
     setTimeout(() => {
       preloader.style.display = 'none';
-    }, 2000); // 2,2s = duração da animação + pequeno delay
+    }, 100); // 2,2s = duração da animação + pequeno delay
   });
